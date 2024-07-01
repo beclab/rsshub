f084fed8c18f68da192307cdf1f7f6600b4ed9b0
simon <ljx1680535@163.com>
2024-07-01 09:26:45 +0800

    rsshub search

--- src/rss/rss.controller.ts
+++ src/rss/rss.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Query, Logger } from '@nestjs/common';
-
+import { getWebsiteRSSHub, getPageRSSHub } from './radar/radar';
+import { defaultRules } from './radar/radar-rules';
 import { RSSService } from './rss.service';
 
 @Controller('/rss')
@@ -11,5 +12,30 @@ export class RSSController {
   @Get('/')
   async get_rss(@Query('path') path: string): Promise<string> {
     return await this.rssService.get_rss(path);
+  }
+
+  @Get('/rsshub/website')
+  async checkRssHubWebSite(@Query('url') url: string): Promise<any> {
+    this.logger.debug('rsshub');
+    const rsshubResult = getWebsiteRSSHub({
+      url,
+      rules: defaultRules,
+    });
+    this.logger.debug('rss Hub web site len:', rsshubResult.length);
+    return rsshubResult;
+  }
+
+  @Get('/rsshub/page')
+  async checkRssHubInPage(
+    @Query('url') url: string,
+    @Query('html') html: string,
+  ): Promise<any> {
+    const rsshubResult = getPageRSSHub({
+      url,
+      html,
+      rules: defaultRules,
+    });
+    this.logger.debug('rssHub  in page  len:', rsshubResult.length);
+    return rsshubResult;
   }
 }
